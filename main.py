b3c7f2954f7fc36bf5a50711d26892b7a29e3f1b
rotimijoseph <rotimi.joseph@hotmail.com>
2026-05-10 22:48:33 +0100

    add minor changes to main

--- main.py
+++ main.py
@@ -4,5 +4,6 @@ df = pd.read_json("data/events/7298.json")
 print(df.head())
 print(df.columns)
 
+# TODO: updates
 # want if event name includes either WFC, LFC, or Women's then keep in the data else want to delete the data files 
-
+# to look for: passes, shots, players name, locations(x, y)
